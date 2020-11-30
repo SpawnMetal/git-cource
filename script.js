dfe9ff45e4951b69a0fb3d22bad31c895c4d8d6e
SpawnMetal <46967498+SpawnMetal@users.noreply.github.com>
2020-11-30 21:48:26 +0700

    commit 2

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 console.log('Test 2')
 console.log('checkout test')
 console.log('push test - develop')
+console.log('push test - develop 2')
